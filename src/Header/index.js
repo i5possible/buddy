aef4d37a0c6fd628111df6fcb08ff5b8e4476812
i5possible <lianghongbuaa@gmail.com>
2019-11-18 21:46:36 +0800

    adjust style

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,18 +2,32 @@ import React from 'react';
 import './index.css';
 import { Link } from "react-router-dom";
 
-const Header = ({ }) => {
-    const navigation = <ul className='nav'>
+const Navigation = () => <div className='nav-wrapper'>
+    <div className='nav'>
         <li className='tab'>
             <Link to="/" className='link'>Home</Link>
         </li>
         <li className='tab'>
             <Link to="/mini-habit" className='link'>Mini Habit</Link>
         </li>
-    </ul>;
+        <li className='tab'>
+        </li>
+        <li className='tab'>
+        </li>
+    </div>
+</div>;
+
+const Icon = () => <img src='logo192.png' className='logo' alt='BUDDY'></img>
+
+const Header = ({ }) => {
+
+
     return (
         <div className='header' >
-            { navigation }
+            <div className='header-content'>
+                <Icon />
+                <Navigation />
+            </div>
         </div>
     );
 };
