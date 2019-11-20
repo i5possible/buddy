6121f398921ef04b6350162a53d5c0a38be5b0c0
i5possible <lianghongbuaa@gmail.com>
2019-11-20 21:23:19 +0800

    add icon

---
+++ src/Content/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Content = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Content.propTypes = {
+
+};
+
+export default Content;
