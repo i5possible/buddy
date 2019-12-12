087fb103bffc67a0eadc5c0b628977868e1bcbb0
i5possible <lianghongbuaa@gmail.com>
2019-12-12 19:36:17 +0800

    update style

--- src/Component/MiniHabit/Item.js
+++ src/Component/MiniHabit/Item.js
@@ -3,11 +3,19 @@ import './Item.css';
 
 
 const Item = ({ item, onClick, onDeleteClick }) => {
+
+    const frequencyMap = {
+        'daily': 'D',
+        'weekly': 'W',
+        'monthly': 'M',
+    }
+
     return (
         <div className='item-wrapper'>
+            <span className="frequency">{frequencyMap[item.frequency]}</span> 
             <button className={item.completed ? 'text-button checked' : 'text-button'}
                 onClick={() => onClick(item)}> 
-                {item.name} 
+                <span>{item.name}</span>
             </button>
             <span className="deleteIcon" onClick={() => onDeleteClick(item.id)}>X</span>
         </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,7 @@ import rootReducer from "./reducers";
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
-const loggerMiddleware = createLogger()
-
+const loggerMiddleware = createLogger();
 
 export default createStore(rootReducer, composeWithDevTools(
     applyMiddleware(thunkMiddleware, loggerMiddleware)
